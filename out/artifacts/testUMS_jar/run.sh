4565f5132da79ab2f682f3ded8a8b63c473dc51b
zawster <p176142@nu.edu.pk>
2019-12-13 18:41:59 +0500

    Conatains .jar file of Whole Project

---
+++ out/artifacts/testUMS_jar/run.sh
@@ -0,0 +1,1 @@
+java --module-path /home/zawster/Downloads/Softwares/openjfx-11.0.2_linux-x64_bin-sdk/javafx-sdk-11.0.2/lib --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web  -jar testUMS.jar
