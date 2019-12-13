6336a0117b014dacf1f5f800cfe4f37f5b7da8c9
zawster <p176142@nu.edu.pk>
2019-12-13 19:03:23 +0500

    Faculty Controller which contains java files

--- src/fController/attendenceFaculty.java
+++ src/fController/attendenceFaculty.java
@@ -1,5 +1,4 @@
 package fController;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -21,7 +20,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
-
+// Faculty Attendence
 public class attendenceFaculty implements Initializable {
 
 
